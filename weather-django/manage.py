d5d7763b956bd4257e665de9a3f7c3b2eeb07b11
Vicky Kumar <imvickykumar999@gmail.com>
2024-01-30 22:01:01 +0530

    pip install Django -U

--- weather-django/manage.py
+++ weather-django/manage.py
@@ -19,3 +19,23 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+rf'''
+python -m venv env
+env\Scripts\activate
+
+env\Scripts\python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
+
+python manage.py migrate
+python manage.py createsuperuser --username imvickykumar999 --email imvickykumar999@gmail.com
+
+python manage.py runserver
+
+http://127.0.0.1:8000
+http://127.0.0.1:8000/admin
+
+# Ctrl + C to exit 
+deactivate
+'''
